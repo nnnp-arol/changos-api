e9275611de6808d286901bf83db1f2ff2a17311e
nnnp-arol <arol.alonso@nonerds.com>
2023-05-21 02:04:52 -0300

    first commit

---
+++ src/models/task.model.js
@@ -0,0 +1,19 @@
+import mongoose, { SchemaType } from "mongoose";
+
+const { Schema } = mongoose;
+
+const TaskSchema = new Schema(
+  {
+    ticket: { type: String, required: true },
+    type: { type: String },
+    description: { type: String },
+    done: { type: Boolean, default: false },
+    enviroment: { type: String },
+    dev: { type: String },
+    jira: { type: String },
+    sprint: { type: String },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const Task = mongoose.model("Task", TaskSchema, "tasks");
---
+++ src/routes/task.route.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { Task } from "../models/task.model.js";
+
+export const taskRouter = express.Router();
+
+taskRouter.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+taskRouter.post("/task", async (req, res) => {
+  const { ticket, type, description, done, enviroment, dev, jira, sprint } =
+    req.body;
+  const foundTask = await Task.findOne({ ticket });
+  if (foundTask) {
+    return res.send("task is already exist");
+  }
+  const newTask = new Task({
+    ticket,
+    type,
+    description,
+    done,
+    enviroment,
+    dev,
+    jira,
+    sprint,
+  });
+  await newTask.save();
+  res.send("task created successfully");
+});
+
+taskRouter.delete("/task/:id", async (req, res) => {
+  await Task.deleteOne({ _id: req.params.id });
+  res.send("Task deleted successfully");
+});
+
+taskRouter.put("/task/:id", async (req, res) => {
+  const editedTask = await Task.updateOne(
+    { _id: req.params.id },
+    { ...req.body }
+  );
+  return editedTask;
+});
